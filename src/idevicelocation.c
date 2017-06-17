4b3f207a1203005b5ea24e9968832054a2886103
Jon Gabilondo <jon.gabilondo@hpe.com>
2017-06-17 15:45:59 +0300

    Commands line arguments improved

--- src/idevicelocation.c
+++ src/idevicelocation.c
@@ -11,12 +11,12 @@
 #include <string.h>
 #include <getopt.h>
 #include <errno.h>
-#include <time.h>
-#include <libgen.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <sys/stat.h>
-#include <dirent.h>
+//#include <time.h>
+//#include <libgen.h>
+//#include <inttypes.h>
+//#include <limits.h>
+//#include <sys/stat.h>
+//#include <dirent.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -31,46 +31,21 @@
 
 #define swap32(value) (((value & 0xFF000000) >> 24) | ((value & 0x00FF0000) >> 8) | ((value & 0x0000FF00) << 8) | ((value & 0x000000FF) << 24) )
 
-char *lat = NULL;
-char *lng = NULL;
 char *udid = NULL;
-char *options = NULL;
-
-enum cmd_mode {
-    CMD_NONE = 0,
-    CMD_LIST_APPS,
-    CMD_INSTALL,
-    CMD_UNINSTALL,
-    CMD_UPGRADE,
-    CMD_LIST_ARCHIVES,
-    CMD_ARCHIVE,
-    CMD_RESTORE,
-    CMD_REMOVE_ARCHIVE
-};
-
-int cmd = CMD_NONE;
-
-char *last_status = NULL;
-int wait_for_command_complete = 0;
-int notification_expected = 0;
-int is_device_connected = 0;
-int command_completed = 0;
-int err_occurred = 0;
 
 static void print_usage(int argc, char **argv)
 {
 	char *name = NULL;
 
 	name = strrchr(argv[0], '/');
-	printf("Usage: %s OPTIONS\n", (name ? name + 1 : argv[0]));
-	printf("Set Geo Location in iOS devices.\n\n");
+    printf("Set Geo Location in iOS devices.\n\n");
+	printf("Usage: %s [OPTIONS] LATITUDE LONGITUDE\n\n", (name ? name + 1 : argv[0]));
+    printf(" The following OPTIONS are accepted:\n");
 	printf
 		("  -u, --udid UDID\tTarget specific device by its 40-digit device UDID.\n"
          "  -h, --help\t\tprints usage information\n"
-         "  -l, --lat\t\tlatitude\n"
-         "  -n, --lng\t\tlongitude\n"
 		 "  -d, --debug\t\tenable communication debugging\n" "\n");
-	printf("Homepage: <http://libimobiledevice.org>\n");
+	printf("Homepage: <https://github.com/JonGabilondoAngulo>\n");
 }
 
 static void parse_opts(int argc, char **argv)
@@ -79,45 +54,41 @@ static void parse_opts(int argc, char **argv)
 		{"help", no_argument, NULL, 'h'},
 		{"udid", required_argument, NULL, 'u'},
         {"debug", no_argument, NULL, 'd'},
-        {"lat", required_argument, NULL, 'l'},
-        {"lng", required_argument, NULL, 'n'},
 		{NULL, 0, NULL, 0}
 	};
 	int c;
 
 	while (1) {
-        c = getopt_long(argc, argv, "hdu:l:n:", longopts, (int *) 0);
+        c = getopt_long(argc, argv, "hdu:", longopts, (int *) 0);
 		if (c == -1) {
 			break;
 		}
-        
-		switch (c) {
-            case 'l':
-                lat = strdup(optarg);
-                break;
-            case 'n':
-                lng = strdup(optarg);
-                break;
+        switch (c) {
             case 'h':
                 print_usage(argc, argv);
-                exit(0);
-		case 'd':
-			idevice_set_debug_level(1);
-			break;
-		default:
-			print_usage(argc, argv);
-			exit(2);
-		}
+                exit(EXIT_SUCCESS);
+            case 'u':
+                if (strlen(optarg) != 40) {
+                    printf("%s: invalid UDID specified (length != 40)\n", argv[0]);
+                    print_usage(argc, argv);
+                    exit(2);
+                }
+                udid = strdup(optarg);
+                break;
+            case 'd':
+                idevice_set_debug_level(1);
+                break;
+            default:
+                print_usage(argc, argv);
+                exit(2);
+        }
 	}
-
-//    if (cmd == CMD_NONE) {
-//        printf("ERROR: No mode/command was supplied.\n");
-//    }
     
-//    if (cmd == CMD_NONE || optind <= 1 || (argc - optind > 0)) {
-//        print_usage(argc, argv);
-//        exit(2);
-//    }
+    if ((argc - optind) < 2) {
+        printf("ERROR: You need to specify LATITUDE and LONGITUDE\n\n");
+        print_usage(argc, argv);
+        exit(EXIT_FAILURE);
+    }
 }
 
 
@@ -127,16 +98,18 @@ int main(int argc, char **argv)
     lockdownd_client_t client = NULL;
     service_client_t service_client = NULL;
     lockdownd_service_descriptor_t service = NULL;
+    char *lat = NULL;
+    char *lng = NULL;
 	int res = 0;
     
 	parse_opts(argc, argv);
-
-	argc -= optind;
-	argv += optind;
-
+    
+    lat = (argv+optind)[0];
+    lng = (argv+optind+1)[0];
+    
 	if (IDEVICE_E_SUCCESS != idevice_new(&phone, udid)) {
 		fprintf(stderr, "No iOS device found, is it plugged in?\n");
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	if (
@@ -150,15 +123,12 @@ int main(int argc, char **argv)
     if ((lockdownd_start_service(client, "com.apple.dt.simulatelocation",  &service) != LOCKDOWN_E_SUCCESS) || !service) {
              fprintf(stderr,  "Could not start com.apple.dt.simulatelocation!\n");
              goto leave_cleanup;
-    } else {
-        printf("SUCCESS: Service opened com.apple.dt.simulatelocation.\n");
     }
     
     service_error_t se = service_client_new(phone, service, &service_client);
     if (se) {
         printf("ERROR: Could not crate Service Client.\n");
     } else {
-        printf("SUCCESS: Service Client created.\n");
         
         uint32_t start = 0;
         uint32_t stop = swap32(1);
@@ -189,19 +159,6 @@ int main(int argc, char **argv)
     service = NULL;
     
 	setbuf(stdout, NULL);
-
-	if (last_status) {
-		free(last_status);
-		last_status = NULL;
-	}
-	notification_expected = 0;
-    
-//    else {
-//        printf
-//            ("ERROR: no command selected?! This should not be reached!\n");
-//        res = -2;
-//        goto leave_cleanup;
-//    }
     
 	if (client) {
 		/* not needed anymore */
@@ -215,16 +172,9 @@ leave_cleanup:
 	}
 	idevice_free(phone);
 
-	if (udid) {
-		free(udid);
-	}
-	if (options) {
-		free(options);
-	}
-
-	if (err_occurred && !res) {
-		res = 128;
-	}
-
+    if (udid) {
+        free(udid);
+    }
+    
 	return res;
 }
